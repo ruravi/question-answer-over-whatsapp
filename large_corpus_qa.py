a9cf4669423b66a9572d91522056af71316e9642
Rukmani Ravi <lakhi.r@gmail.com>
2023-05-05 18:50:08 -0700

    Write a test for the large qa

---
+++ large_corpus_qa.py
@@ -0,0 +1,103 @@
+from typing import Optional, List
+from langchain.llms.base import LLM
+from langchain import PromptTemplate
+from langchain.llms.openai import OpenAI
+from langchain.text_splitter import CharacterTextSplitter
+from transformers import GPT2TokenizerFast
+from langchain.vectorstores import Chroma
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.schema import BaseRetriever
+from langchain.chains import RetrievalQA
+from langchain.document_loaders import WhatsAppChatLoader
+from langchain.docstore.document import Document
+import logging
+
+
+class LargeCorpusQA:
+    def __init__(
+        self,
+        model: LLM = OpenAI(temperature=0),
+        options: dict = {
+            "chunk_size": 100,
+            "persist_directory": "vector_index",
+        },
+    ) -> None:
+        self.model = model
+        self.CHUNK_SIZE = options["chunk_size"]
+        self.embeddings = HuggingFaceEmbeddings()
+        self.PERSIST_DIRECTORY = options["persist_directory"]
+        self.qa_bot = None
+
+    def initialize_vector_store(self, file_path: Optional[str]) -> None:
+        if self.qa_bot is not None:
+            logging.warning("Vector store already initialized")
+            return
+        retriever = (
+            self.create_new_vector_store(file_path)
+            if file_path
+            else self.load_vector_store()
+        )
+        self.qa_bot = RetrievalQA.from_chain_type(
+            llm=self.model,
+            chain_type="stuff",
+            retriever=retriever,
+            chain_type_kwargs={
+                "prompt": self.__form_prompt_template(),
+            },
+        )
+
+    def answer(self, query: str) -> str:
+        if self.qa_bot is None:
+            raise ValueError("Vector store not initialized")
+        return self.qa_bot.run(query)
+
+    def load_vector_store(self) -> BaseRetriever:
+        logging.info("Loading pre-existing vector store")
+        vector_store = Chroma(
+            persist_directory=self.PERSIST_DIRECTORY, embedding_function=self.embeddings
+        )
+        return vector_store.as_retriever()
+
+    def create_new_vector_store(self, file_path: str) -> BaseRetriever:
+        logging.info("Creating new vector store")
+
+        unchunked_documents = self.load_documents(file_path)
+        chunked_documents = self.chunk_documents(unchunked_documents)
+        fresh_vector_db = self.create_db(chunked_documents)
+
+        return fresh_vector_db.as_retriever()
+
+    def load_documents(self, file_path: str):
+        document_loader = WhatsAppChatLoader(file_path)
+        unchunked_documents = document_loader.load()
+        return unchunked_documents
+
+    def chunk_documents(self, documents: List[Document]) -> List[Document]:
+        tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
+        text_splitter = CharacterTextSplitter.from_huggingface_tokenizer(
+            tokenizer, chunk_size=self.CHUNK_SIZE, chunk_overlap=0
+        )
+        return text_splitter.split_documents(documents)
+
+    def create_db(self, documents: List[Document]):
+        fresh_vector_db = Chroma.from_documents(
+            documents,
+            self.embeddings,
+            persist_directory=self.PERSIST_DIRECTORY,
+        )
+        fresh_vector_db.persist()
+        return fresh_vector_db
+
+    def __form_prompt_template(self):
+        prompt_template = """
+            Read the following transripts of a several chats between two or more persons.
+            Answer the question below correctly. Provide an explanation for your answer.
+            Where possible cite the relevant part of the transcript.
+            
+            {context}
+
+            Question: {question}
+            """
+        return PromptTemplate(
+            template=prompt_template, input_variables=["context", "question"]
+        )
--- test_simple_qa.py
+++ test_simple_qa.py
@@ -1,12 +1,16 @@
 import unittest
 from langchain.llms.fake import FakeListLLM
 from simple_qa import SimpleQA
+from large_corpus_qa import LargeCorpusQA
 
 
 class TestSimpleQA(unittest.TestCase):
+    def setUp(self) -> None:
+        self.fake_model = FakeListLLM(responses=["I am a human.", "I am a robot."])
+        return super().setUp()
+
     def test_simple_qa(self):
-        fake_model = FakeListLLM(responses=["I am a human.", "I am a robot."])
-        sut = SimpleQA(fake_model)
+        sut = SimpleQA(self.fake_model)
         actual_answer = sut.answer(
             chat="""
         [7/9/18, 6:53:47 PM] Jane Doe: Ok bye , will call in an hour
@@ -15,3 +19,29 @@ class TestSimpleQA(unittest.TestCase):
             question="Are you a human?",
         )
         self.assertEqual("I am a human.", actual_answer)
+
+    def test_large(self):
+        sut = LargeCorpusQA(self.fake_model)
+        sut.initialize_vector_store(None)
+        actual_answer = sut.answer("Are you a human?")
+        self.assertEqual("I am a human.", actual_answer)
+
+    def test_large_no_store(self):
+        sut = LargeCorpusQA(self.fake_model)
+        with self.assertRaises(ValueError):
+            sut.answer("Are you a human?")
+
+    def test_load_whatsapp_chats(self):
+        sut = LargeCorpusQA(
+            self.fake_model,
+            options={"chunk_size": 20, "persist_directory": "test_vector_index"},
+        )
+        actual_unchunked_documents = sut.load_documents("data/whatsapp_export_test.txt")
+        self.assertEqual(1, len(actual_unchunked_documents))
+
+        actual_chunked_docs = sut.chunk_documents(actual_unchunked_documents)
+        self.assertEqual(4, len(actual_chunked_docs))
+
+        actual_db = sut.create_db(actual_chunked_docs)
+        actual_results = actual_db.search("sunglasses", search_type="similarity")
+        self.assertEqual(4, len(actual_results))
